44fc557bfdf47f590925db3523713f1e1a1ac203
Badal Virmani <badalvirmani1998@gmail.com>
2023-08-25 13:18:16 +0530

    Redux ,  Npm and API
    I  used Redux to display data on frontend which is saved in backend (mongoDB). In Redux , i installed redux , thunk , dev-tools-extension  through npm(node  package manager) and also create  a get api in route.js file and controller.

---
+++ client/src/redux/store.js
@@ -0,0 +1,19 @@
+import {  createStore , combineReducers , applyMiddleware} from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import {getProductsReducer} from 'redux';
+import  {thunk} from 'redux-thunk';
+
+
+const reducer = combineReducers({
+    getProducts : getProductsReducer
+});
+
+const middleware =  [thunk];
+
+const store = createStore(
+    reducer,
+    composeWithDevTools (applyMiddleware(...middleware))
+
+);
+
+export default  store;
--- server/routes/route.js
+++ server/routes/route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { userSignup , userLogin } from '../controller/user-constroller.js';
+import { userSignup , userLogin  , getProducts} from '../controller/user-constroller.js';
 
 
 const router = express.Router();
@@ -7,5 +7,7 @@ const router = express.Router();
 
 router.post('/signup', userSignup);
 router.post('/login', userLogin);
+
+router.get('/products' , getProducts);
 
 export default router;
